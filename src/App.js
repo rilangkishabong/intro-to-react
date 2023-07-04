0c6217dee5317662e7bfe368200214f6a2d58873
Rilangki <rilangkishabong@gmail.com>
2023-07-04 10:43:19 +0530

    counter component implimented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Counter } from "./components/counter/Counter";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+   counter===3 && alert("counter-value is 3")
+  }, [counter])
+  
   return (
-    <p>
-      <div className="App">Hello World!</div>
-      <div>hello you</div>
-    </p>
+    <div style={{textAlign: "center"}}>
+    {counter>0 && <Counter counter={counter}/>}
+    <div>
+      <button onClick={()=> setCounter(counter+1) }>Increase</button>
+      <button onClick={()=> setCounter(counter-1) }>Decrease</button>
+    </div>
+    </div>
   );
 }
 
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Counter = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
